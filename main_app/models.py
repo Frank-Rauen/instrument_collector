6f36c5b8e27d19df49b9eb5a360cff1e6a11fec7
Frank Rauen <home@Franks-MacBook-Air.local>
2020-04-07 10:54:17 -0600

    adds index models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Instrument:  
+  def __init__(self, make, model, year, description):
+    self.make = make
+    self.model = model
+    self.year = year
+    self.description = description
+
+instruments = [
+    Instrument('Ibanez', 'RG500', 2007, '6 String Shred Machine'),
+    Instrument('Gibson', 'Les Paul', 2007, '6 String Shred Machine'),
+    Instrument('Fender', 'Stratocaster', 2007, '6 String Shred Machine'),
+    ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Instrument, instruments
 
 # Create your views here.
+def home(request):
+    return render(request, 'base.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def instruments_index(request):
+      return render(request, 'instruments/index.html', { 'instruments' : instruments })
